9dbfac45b10ab5393dbd538f3417fa3d853be96f
Florian Hermann <florian.hermann94@gmail.com>
2019-08-02 22:28:04 +0200

    Add comment on Action interface

--- src/Action/ActionInterface.php
+++ src/Action/ActionInterface.php
@@ -9,5 +9,13 @@ use Psr\Http\Message\ServerRequestInterface;
 
 interface ActionInterface
 {
+    /**
+     * The action's method itself.
+     *
+     * @param ServerRequestInterface $request
+     * @param array $arguments Arguments from the routing system
+     *
+     * @return ResponseInterface This method should always return a Response
+     */
     public function process(ServerRequestInterface $request, array $arguments): ResponseInterface;
 }
