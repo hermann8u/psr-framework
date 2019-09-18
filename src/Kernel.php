b707b791c0c14eaaaae095291e462400ec76c178
Florian Hermann <florian.hermann94@gmail.com>
2019-09-18 20:55:39 +0200

    Reorder the kernel boot method

--- src/Kernel.php
+++ src/Kernel.php
@@ -58,12 +58,12 @@ final class Kernel
     {
         $containerDumpFile = $this->getCacheDir().'/container.php';
 
-        if ($this->debug || !file_exists($containerDumpFile)) {
-            $this->buildContainer($containerDumpFile);
-        }
-
         if ($this->debug) {
             $this->initWhoops();
+        }
+
+        if ($this->debug || !file_exists($containerDumpFile)) {
+            $this->buildContainer($containerDumpFile);
         }
 
         require_once $containerDumpFile;
