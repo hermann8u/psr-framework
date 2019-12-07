581ce8b9296c0329fbf0cf1f3396045fbb9072d4
Florian Hermann <florian.hermann94@gmail.com>
2019-12-07 20:07:14 +0100

    Install error handle

--- public/index.php
+++ public/index.php
@@ -4,5 +4,11 @@ declare(strict_types=1);
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
-$kernel = new App\Kernel($_SERVER['APP_ENV'], $_SERVER['APP_DEBUG']);
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    \Symfony\Component\ErrorHandler\Debug::enable();
+}
+
+$kernel = new \App\Kernel($_SERVER['APP_ENV'], $_SERVER['APP_DEBUG']);
 $kernel->run();
